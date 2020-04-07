b0182b106cc05f286950c6b5e00e820923ca1cee
Jéssica Carneiro <jessica.carneiro@thoughtworks.com>
2020-04-07 18:58:28 -0300

    First test case passing: minimum code to create an account and make a deposit

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,20 +1,21 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    private double balance;
 
     public Account() {
 
     }
 
     public Account(double value) {
-
+        this.balance = value;
     }
 
     public void deposit(double value) {
-
+        this.balance += value;
     }
 
     public double getBalance() {
-        return 0;
+        return this.balance;
     }
 }
