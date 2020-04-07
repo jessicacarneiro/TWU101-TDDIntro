e978203ea35ec2f4f6e141d196efb98ae8452563
Jéssica Carneiro <jessica.carneiro@thoughtworks.com>
2020-04-07 18:56:52 -0300

    First failing test case: balance should increase when deposit is made.

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,20 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+
+    public Account() {
+
+    }
+
+    public Account(double value) {
+
+    }
+
+    public void deposit(double value) {
+
+    }
+
+    public double getBalance() {
+        return 0;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,11 +3,19 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+        // given
+        Account account = new Account(100);
+
+        // when
+        account.deposit(50);
+
+        // then
+        assertEquals(150.0, account.getBalance());
     }
 
     @Test
