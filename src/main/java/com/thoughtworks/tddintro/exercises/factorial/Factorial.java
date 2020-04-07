33b1cdc259424e9f3e8f0d181e9168751afe7ced
Jéssica Carneiro <jessica.carneiro@thoughtworks.com>
2020-04-07 18:42:48 -0300

    Considering invalid negative numbers.

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,10 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        }
+
         if (i == 1 || i == 0) {
             return 1;
         }
